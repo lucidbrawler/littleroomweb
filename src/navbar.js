dbac2937c22345cc2d834ba3bbe2431e4307e9c3
Kenneth Sinclair <kennethpsinclair@yahoo.com>
2023-03-11 17:00:29 -0500

    added loose contact, color on just the toggler

--- src/navbar.js
+++ src/navbar.js
@@ -8,12 +8,12 @@ import Offcanvas from 'react-bootstrap/Offcanvas';
 
 function OffcanvasExample() {
   return (
-    <>
+    <div>
       {[false].map((expand) => (
-        <Navbar  key={expand} bg="light" expand={expand} className="mb-3 ">
-          <Container className='colort' fluid>
+        <Navbar  key={expand} bg="" expand={expand} className="mb-3 ">
+          <Container  fluid>
             <Navbar.Brand href="/"><img href="/" className="logo" alt=""src={logo}/></Navbar.Brand>
-            <Navbar.Toggle    aria-controls={` offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Toggle className='colort'   aria-controls={` offcanvasNavbar-expand-${expand}`} />
             <Navbar.Offcanvas 
               id={`offcanvasNavbar-expand-${expand}`}
               aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
@@ -49,7 +49,7 @@ function OffcanvasExample() {
           </Container>
         </Navbar>
       ))}
-    </>
+   </div>
   );
 }
 
